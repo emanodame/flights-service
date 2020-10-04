4b68859d24107c191c2cf812821ab13578eea0c9
emanodame <emanodame1@gmail.com>
2020-10-04 01:40:53 +0100

    init flights-service project

---
+++ src/main/java/com/ryanair/flights/service/flightsservice/FlightsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ryanair.flights.service.flightsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryanair/flights/service/flightsservice/FlightsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanair.flights.service.flightsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
